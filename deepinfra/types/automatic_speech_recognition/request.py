3a3a176c2dcaf6bdbf46d41bb50fa779702155e2
Oguz Vuruskaner <ovuruska@outlook.com>
2024-04-25 16:26:53 +0300

    Removed redundant comments

--- deepinfra/types/automatic_speech_recognition/request.py
+++ deepinfra/types/automatic_speech_recognition/request.py
@@ -1,23 +1,3 @@
-"""
-import {ReadStreamInput} from "@/lib/utils/read-stream";
-
-export interface AutomaticSpeechRecognitionRequest {
-  audio: ReadStreamInput;
-  task?: "transcribe" | "translate";
-  language?: string;
-  temperature?: number;
-  patience?: number;
-  suppress_tokens?: string;
-  initial_prompt?: string;
-  condition_on_previous_text?: boolean;
-  temperature_increment_on_fallback?: number;
-  compression_ratio_threshold?: number;
-  logprob_threshold?: number;
-  no_speech_threshold?: number;
-  webhook?: string;
-}
-
-"""
 from dataclasses import dataclass
 from typing import Optional
 
