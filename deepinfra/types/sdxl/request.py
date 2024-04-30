60c181f27a9fc59d3ce854b4f68d34e2aa4037d7
Oguz Vuruskaner <ovuruska@outlook.com>
2024-04-30 16:01:40 +0300

    refactor(SDXL): better reusability

--- deepinfra/types/sdxl/request.py
+++ deepinfra/types/sdxl/request.py
@@ -2,22 +2,9 @@ from typing import Optional
 
 from dataclasses import dataclass
 
+from deepinfra.types.text_to_image import TextToImageRequest
+
 
 @dataclass
 class SdxlRequest:
-    prompt: str
-    negative_prompt: Optional[str] = None
-    image: Optional[str] = None
-    mask: Optional[str] = None
-    width: Optional[int] = None
-    height: Optional[int] = None
-    num_outputs: Optional[int] = None
-    scheduler: Optional[str] = None
-    num_inference_steps: Optional[int] = None
-    guidance_scale: Optional[float] = None
-    prompt_strength: Optional[float] = None
-    seed: Optional[int] = None
-    refine: Optional[str] = None
-    high_noise_frac: Optional[float] = None
-    refine_steps: Optional[int] = None
-    apply_watermark: Optional[bool] = True
+    input: TextToImageRequest
